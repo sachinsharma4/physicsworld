c87df947bf35fb9c46fead66a8775fa3043d6f48
sachinsharma4 <66316872+sachinsharma4@users.noreply.github.com>
2021-05-19 10:38:21 +0530

    Update sw10.js

--- sw10.js
+++ sw10.js
@@ -1,3 +1,29 @@
+
+// Choose a cache name
+const cacheName = 'cache-v1';
+// List the files to precache
+const precacheResources = [
+'/', 
+'index.html',
+'physics.html',
+'biology.html',
+'math.html',
+'chemistry.html',
+'about.html'
+];
+
+// When the service worker is installing, open the cache and add the precache resources to it
+self.addEventListener('install', (event) => {
+    
+ 
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service worker activate event!');
+});
+
+// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
 ]
 // Choose a cache name
 const cacheName = 'cache-v1';
@@ -5,6 +31,9 @@ const cacheName = 'cache-v1';
 const precacheResources = [
 '/', 
 'index.html',
+'computerscience.html',
+'rachelcarson.png',
+'logo.png',
 'physics.html',
 'biology.html',
 'math.html',
